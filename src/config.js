28fda15a33e2e8acb0c53efd66ca8e38411c9b1e
Pedro Fernandes <1060503@isep.ipp.pt>
2018-01-10 15:02:54 +0000

    changed request http -> https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
         CLIENT_ID: "zueqkcAbR5IusJ0MVKvpLysj2VD4aUaQ",
         CLIENT_SECRET: "ZKJfrwZ5LQ_3_Kv9D48z9YOCXePzIUOjRVoZDD0B9LyTNgvFtnmK0_MlL3vMeQHD",
-        orders_url: "http://lapr5-g6618-orders-management.azurewebsites.net/"
+        orders_url: "https://lapr5-g6618-orders-management.azurewebsites.net/"
 }
