b571d3352388f5cbadd6c839fa22a5381d1357d0
FlavioRelvas <1140826@isep.ipp.pt>
2018-01-12 02:44:58 +0000

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import SweetAlert from 'react-bootstrap-sweetalert';
 import Button from 'elements/CustomButton/CustomButton.jsx';
 
-class Footer extends Component {  
-    constructor(props){
+class Footer extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             alert: null,
@@ -11,46 +11,46 @@ class Footer extends Component {
         }
         this.hideAlert = this.hideAlert.bind(this);
     }
-    basicAlert(){
+    basicAlert() {
         this.setState({
             alert: (
                 <SweetAlert
-                    style={{display: "block",marginTop: "-100px"}}
+                    style={{ display: "block", marginTop: "-100px" }}
                     title="Authors"
                     onConfirm={() => this.hideAlert()}
                     onCancel={() => this.hideAlert()}
                     confirmBtnBsStyle="info"
-                   
+
                 >
-                <ul>
-                    <li>Daniel Gonçalves (1151452)</li>
-                    <li>Diana Silva (1151088)</li>
-                    <li>Flávio Relvas (1140826)</li>
-                    <li>Ivo Ferro (1151159)</li>
-                    <li>Tiago Correia (1151031)</li>
-                    <li>Pedro Fernandes (1060503)</li>
-                    <li>Renato Oliveira (1150822)</li>
-                </ul>
+                    <ul className="list-unstyled">
+                        <li>Pedro Fernandes (1060503)</li>
+                        <li>Renato Oliveira (1140822)</li>
+                        <li>Flávio Relvas (1140826)</li>
+                        <li>Tiago Correia (1151031)</li>
+                        <li>Diana Silva (1151088)</li>
+                        <li>Ivo Ferro (1151159)</li>
+                        <li>Daniel Gonçalves (1151452)</li>
+                    </ul>
                 </SweetAlert>
-                
+
             )
         });
     }
-    hideAlert(){
+    hideAlert() {
         this.setState({
             alert: null
         });
     }
 
-    render(){
+    render() {
         return (
-            <footer className={"footer" + (this.props.transparent !== undefined ? " footer-transparent":"")}>
-                <div className={"container" + (this.props.fluid !== undefined ? "-fluid":"")}>{this.state.alert}
+            <footer className={"footer" + (this.props.transparent !== undefined ? " footer-transparent" : "")}>
+                <div className={"container" + (this.props.fluid !== undefined ? "-fluid" : "")}>{this.state.alert}
                     <p className="copyright pull-right">
                         &copy; Orders Management Front-End -  {1900 + (new Date()).getYear()} <a>LAPR5-6618</a>
-                    &nbsp; &nbsp;<Button onClick={this.basicAlert.bind(this)}>Authors</Button>
-                 </p>
-                </div>                              
+                        &nbsp; &nbsp;<Button onClick={this.basicAlert.bind(this)}>Authors</Button>
+                    </p>
+                </div>
             </footer>
         );
     }
